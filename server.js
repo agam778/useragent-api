1c9238a8fa5aabd92a77b98b5ecd668affc4b0c3
Agampreet Singh <agam778singh@outlook.com>
2021-10-20 11:17:51 +0530

    Add Server Files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.get('/', (req, res) => {
+    res.header("Content-Type",'application/json');
+    res.sendFile(path.join(__dirname, 'data.json'));
+})
+
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:8080`);
+});
