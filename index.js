ada6dea17c396e5e5cc818e65a76e14b281258e2
Agampreet Singh <agam778singh@outlook.com>
2021-10-20 19:22:47 +0530

    Fix index.js for deta.sh
    Add Android UserAgent
    
    Add android endpoint

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'data.json'));
 })
 
+app.get('/android', (req, res) => {
+  res.header("Content-Type",'application/json');
+  res.sendFile(path.join(__dirname, 'android.json'));
+})
+
 app.listen(port, () => {
   console.log(`API listening at http://localhost:8080`);
 });
+
+module.exports = app
