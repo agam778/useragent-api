1e7df67d6938890a58f443d7876c10faea8b0d76
Janek <development@superyu.xyz>
2021-10-21 15:51:17 +0200

    Added all routes
    Using route params

--- index.js
+++ index.js
@@ -1,26 +1,74 @@
-const express = require("express");
-const app = express();
+const app = require("express")();
+const data = require("./data.json");
 const port = 8080;
-const path = require("path");
+
+// returns [data, statusCode]
+function getData(os, browser, androidVer) {
+  if (androidVer != null) {
+    if (data.android[androidVer] == null)
+      return [null, 404];
+    else {
+      if (browser != null) 
+        if (data.android[androidVer][browser] == null)
+          return [null, 404];
+        else
+        return [data.android[androidVer][browser], 200];
+      else
+        return [data.android[androidVer], 200];
+    }
+  }
+
+  if (data[os] == null)
+    return [null, 404];
+
+  if (browser != null) {
+    if (data[os][browser] == null)
+      return [null, 404];
+    else {
+      return [data[os][browser], 200];
+    }
+  }
+
+  return [data[os], 200];
+}
+
+function send(res, dataArray) {
+  if (dataArray[1] == 404)
+    res.sendStatus(404);
+  else {
+    res.header("Content-Type", "application/json");
+    res.send(JSON.stringify(dataArray[0], null, 4));
+  }
+}
 
 app.get("/", (req, res) => {
   res.header("Content-Type", "application/json");
-  res.sendFile(path.join(__dirname, "data/data.json"));
+  res.send(JSON.stringify(data, null, 4));
 });
 
 app.get("/android", (req, res) => {
-  res.header("Content-Type", "application/json");
-  res.sendFile(path.join(__dirname, "data/android.json"));
+  let dataArray = getData("android", null, null);
+  send(res, dataArray);
 });
 
-app.get("/windows", (req, res) => {
-  res.header("Content-Type", "application/json");
-  res.sendFile(path.join(__dirname, "data/windows.json"));
+app.get("/android/:androidVersion", (req, res) => {
+  let dataArray = getData("android", null, req.params.androidVersion);
+  send(res, dataArray);
 });
 
-app.get("/macos", (req, res) => {
-  res.header("Content-Type", "application/json");
-  res.sendFile(path.join(__dirname, "data/macos.json"));
+app.get("/android/:androidVersion/:browser", (req, res) => {
+  let dataArray = getData("android", req.params.browser, req.params.androidVersion);
+  send(res, dataArray);
+});
+
+app.get('/:os', (req, res) => {
+  let dataArray = getData(req.params.os, null, null);
+  send(res, dataArray);
+});
+
+app.get('/:os/:browser', (req, res) => {
+  let dataArray = getData(req.params.os, req.params.browser, null);
+  send(res, dataArray);
 });
 
 app.get("/chromeos", (req, res) => {
