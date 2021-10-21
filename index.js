a75f5b055d469d3694dc82b8d46f1082a5c8d465
Agampreet Singh <agam778singh@outlook.com>
2021-10-21 18:31:32 +0530

    Better code placement

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
-app.get('/', (req, res) => {
-    res.header("Content-Type",'application/json');
-    res.sendFile(path.join(__dirname, 'data.json'));
-})
+app.get("/", (req, res) => {
+  res.header("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname, "data/data.json"));
+});
 
-app.get('/android', (req, res) => {
-  res.header("Content-Type",'application/json');
-  res.sendFile(path.join(__dirname, 'android.json'));
-})
+app.get("/android", (req, res) => {
+  res.header("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname, "data/android.json"));
+});
 
-app.get('/windows', (req, res) => {
-  res.header("Content-Type",'application/json');
-  res.sendFile(path.join(__dirname, 'windows.json'));
-})
+app.get("/windows", (req, res) => {
+  res.header("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname, "data/windows.json"));
+});
 
-app.get('/macos', (req, res) => {
-  res.header("Content-Type",'application/json');
-  res.sendFile(path.join(__dirname, 'macos.json'));
-})
+app.get("/macos", (req, res) => {
+  res.header("Content-Type", "application/json");
+  res.sendFile(path.join(__dirname, "data/macos.json"));
+});
 
 app.listen(port, () => {
   console.log(`API listening at http://localhost:8080`);
 });
 
-module.exports = app
+module.exports = app;
